dc779ff3bf0c561c7c5c7025cc3102f4143433fe
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-28 15:46:30 +0100

    feat: docker compose for postgres database along with environment variables needed

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,4 +3,11 @@ import { get } from 'env-var';
 
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
+
+  //* Database
+  DB_PASSWORD: get('DB_PASSWORD').required().asString(),
+  DB_NAME: get('DB_NAME').required().asString(),
+  DB_HOST: get('DB_HOST').required().asString(),
+  DB_PORT: get('DB_PORT').required().asPortNumber(),
+  DB_USERNAME: get('DB_USER').required().asString(),
 };
