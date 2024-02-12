5edc609ec0f98ce64943f3b6199bb87de1205908
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-12 17:18:22 +0100

    feat: new dto to upload files

---
+++ src/domain/dtos/shared/pagination/pagination.dto.ts
@@ -0,0 +1,17 @@
+import { ToInt } from 'class-sanitizer';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, Min } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @Min(1)
+  @IsNumber()
+  @Type(() => Number)
+  limit?: number;
+
+  @IsOptional()
+  @Min(1)
+  @IsNumber()
+  @Type(() => Number)
+  page?: number;
+}
