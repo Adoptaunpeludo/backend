24b649d41b6722282fb15188bcfcfd7a92c5f2b7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-20 09:15:11 +0100

    feat: third party libraries comments

--- src/config/http-status-codes.adapter.ts
+++ src/config/http-status-codes.adapter.ts
@@ -1,30 +1,51 @@
 import { StatusCodes } from 'http-status-codes';
 
 export class HttpCodes {
-  static get OK() {
+  /**
+   * Returns the HTTP status code for OK (200).
+   */
+  static get OK(): number {
     return StatusCodes.OK;
   }
 
+  /**
+   * Returns the HTTP status code for Created (201).
+   */
   static get CREATED(): number {
     return StatusCodes.CREATED;
   }
 
+  /**
+   * Returns the HTTP status code for Not Found (404).
+   */
   static get NOT_FOUND(): number {
     return StatusCodes.NOT_FOUND;
   }
 
+  /**
+   * Returns the HTTP status code for Bad Request (400).
+   */
   static get BAD_REQUEST(): number {
     return StatusCodes.BAD_REQUEST;
   }
 
+  /**
+   * Returns the HTTP status code for Unauthorized (401).
+   */
   static get UNAUTHORIZED(): number {
     return StatusCodes.UNAUTHORIZED;
   }
 
+  /**
+   * Returns the HTTP status code for Forbidden (403).
+   */
   static get FORBIDDEN(): number {
     return StatusCodes.FORBIDDEN;
   }
 
+  /**
+   * Returns the HTTP status code for Internal Server Error (500).
+   */
   static get INTERNAL_SERVER_ERROR(): number {
     return StatusCodes.INTERNAL_SERVER_ERROR;
   }
--- src/config/uuid.adapter.ts
+++ src/config/uuid.adapter.ts
@@ -1,6 +1,14 @@
 import { validate } from 'uuid';
 
+/**
+ * Utility class for UUID validation.
+ */
 export class UUID {
+  /**
+   * Validates a UUID string.
+   * @param value The UUID string to validate.
+   * @returns A boolean indicating whether the UUID is valid or not.
+   */
   static validate(value: string): boolean {
     return validate(value);
   }
