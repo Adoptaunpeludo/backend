5c1f30513b85eea2c20a247d5a4be580004368c4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-20 09:09:46 +0100

    feat: custom errors comments

--- src/domain/errors/bad-request.error.ts
+++ src/domain/errors/bad-request.error.ts
@@ -1,7 +1,14 @@
 import { HttpCodes } from '../../config/http-status-codes.adapter';
 import { CustomAPIError } from './custom-api.error';
 
+/**
+ * Custom error class representing a BadRequestError.
+ */
 export class BadRequestError extends CustomAPIError {
+  /**
+   * Creates an instance of BadRequestError.
+   * @param message The error message.
+   */
   constructor(public message: string) {
     super('Bad Request', message, HttpCodes.BAD_REQUEST);
   }
--- src/domain/errors/unauthenticated.error.ts
+++ src/domain/errors/unauthenticated.error.ts
@@ -1,8 +1,14 @@
-import { HttpCodes } from "../../config/http-status-codes.adapter";
-import { CustomAPIError } from "./custom-api.error";
+import { HttpCodes } from '../../config/http-status-codes.adapter';
+import { CustomAPIError } from './custom-api.error';
 
-
-export class UnauthenticatedError extends CustomAPIError{
+/**
+ * Represents an unauthenticated error.
+ */
+export class UnauthenticatedError extends CustomAPIError {
+  /**
+   * Creates an instance of UnauthenticatedError.
+   * @param message The error message.
+   */
   constructor(public message: string) {
     super('Unauthenticated', message, HttpCodes.UNAUTHORIZED);
   }
