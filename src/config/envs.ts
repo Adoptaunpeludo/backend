cd28317710c4db456592ab0af60c05b0dea0ee83
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-18 09:03:25 +0200

    feat: docker-compose-api.yml

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -16,12 +16,12 @@ export const envs = {
   JWT_SEED: get('JWT_SEED').required().asString(),
 
   //* Email Service
-  MAIL_SERVICE: get('MAIL_SERVICE').required().asString(),
-  MAILER_EMAIL: get('MAILER_EMAIL').required().asString(),
-  MAILER_SECRET_KEY: get('MAILER_SECRET_KEY').required().asString(),
+  MAIL_SERVICE: get('MAIL_SERVICE').asString(),
+  MAILER_EMAIL: get('MAILER_EMAIL').asString(),
+  MAILER_SECRET_KEY: get('MAILER_SECRET_KEY').asString(),
 
   //*
-  WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
+  WEBSERVICE_URL: get('WEBSERVICE_URL').asString(),
 
   //* RABBITMQ
   RABBITMQ_USER: get('RABBITMQ_USER').required().asString(),
